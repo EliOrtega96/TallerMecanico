7f7c1ad2c20d396d95817867cb3b631ba7f2c90d
Manolo <manolomr221@gmail.com>
2020-05-31 00:05:27 -0500

    validaciones Dom compras

--- compra.php
+++ compra.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
  require_once('conexion.php');
  $id = $_GET['id']; //obtiene el id del registro que se va comprar
  $query = $conn->prepare("SELECT * FROM stock WHERE id_stock ='$id' "); //query para obtener los datos del registro a editar
@@ -10,23 +11,18 @@
                 $producto=$res['nombreProducto'];
                 $desc=$res['descripcion'];
                 $costo="$".$res['costo'].".00";
-               
-
-            }
- 
-
-
-
-           
-           
+            }           
  ?>
-
-
+<?php
+if( isset($_SESSION['id_usuario']) ){
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <title>Document</title>
     <style>
         img
@@ -39,35 +35,67 @@
 <body>
   <div class="contenedor-form">
        <h1>Comprar</h1>
-       <form action="registraCompra.php" method="post" enctype="multipart/form-data">
-       <input style="display: none"  type="text" value="<?php echo $id;?>" name="id_stock"><br>
+       <form action="" method="post" id="form">
+       <input style="display: none" id="id_stock" type="text" value="<?php echo $id;?>" name="id_stock"><br>
       
        <label >Foto: </label><br>
        <img src="images/<?php echo $img ; ?>" alt=""><br>
       
        <label >Producto: </label>
-       <input  type="text" name="nombreProducto" value="<?php echo $producto;?>"  readonly><br>
+       <input  type="text" id="producto" name="nombreProducto" value="<?php echo $producto;?>"  readonly><br>
        <label >Descripcion: </label>
-       <input style="width: 30%" type="text" name="descripcion" value="<?php echo $desc;?>" readonly ><br>
+       <input style="width: 30%" id="descripcion" type="text" name="descripcion" value="<?php echo $desc;?>" readonly ><br>
        <label >Costo: </label>
-       <input  type="text" name="costo" value="<?php echo $costo;?>" readonly><br>
+       <input  type="text" name="costo" id="costo" value="<?php echo $costo;?>" readonly><br>
        <label >Cantidad: </label>
-       <input required type="number" name="cantidad"  min="1" max="300"><br>
+       <input required type="number" id="cantidad" name="cantidad"  min="1" max="300"><br>
        <label for="start">Ingrese fecha:</label>
 
-         <input required type="date" id="start" name="cita"
+         <input required type="date" id="fecha" name="cita"
          value="2020-01-01"
          min="2020-01-01" max="2020-12-31">
-
-         <button type="submit" >Comprar</button>
-      
-       </form>
-      
        
+         <button type="button" id="enviar" >Comprar</button>
+       </form>    
    </div>
     <div id="res"></div>
+<script>
+  $(document).ready(function()
+  {
+    $("button").click(function()
+    {
+        var id_stock=document.getElementById('id_stock').value;
+        var producto=document.getElementById('producto').value;
+        var fecha=document.getElementById('fecha').value;
+        var cantidad=document.getElementById('cantidad').value;
+        var ruta="Id_stock"+id_stock+"&Cantidad"+cantidad+"&Cita"+fecha+"&NombreProducto"+producto;
+        if(cantidad >0){
+            $.ajax({
+            url: 'registraCompra.php',
+            type: 'POST',
+            data: $('#form').serialize(),
+        })
+        .done(function(res){
+            $('#res').html(res)
+        })
+        .fail(function(){
+            console.log("error");
+        })
+        .always(function(){
+            console.log("completo");
+        })
+        }else{
+            alert("debes comprar al menos un producto");
+        } 
+    });
+ });
+</script>
 </body>
 </html>
-
-
+<?php
+}
+else{
+    header("Location: login.php");
+}
+?>
  
--- registraCompra.php
+++ registraCompra.php
@@ -2,74 +2,70 @@
 session_start();
 require_once("conexion.php");
 
-if(!empty($_POST)){
+if(!empty($_POST))
+{
       $stock=$_POST['id_stock'];
       $pedido =$_POST['cantidad']; //
       $cita=$_POST['cita'];
       $id=$_SESSION['id_usuario'];
       $producto=$_POST['nombreProducto'];
       
-     
-
     $query = $conn->prepare("SELECT cantidad FROM stock where id_stock=$stock");   //lee todos los usuarios de la tabla usuarios
     $query->execute(); //ejecuta el query
     $result = $query->fetch();//obtiene dato de piezas
    $total= $result['cantidad'];
 
    if($total>0){
-       
        $restaStock = $total-$pedido;
-       echo $restaStock;
-       if($restaStock >= 0 ){
-           echo "te lo vendo";
+       if($restaStock >= 0 )
+       {
            $val = $conn->prepare("SELECT count(*) as total FROM `agenda` ");   //lee todos los usuarios de la tabla usuarios
            $val->execute();
            $registros = $val->fetch();
 
            $valor= $registros['total'];
-           if($valor == 0){
-            $query2 =$conn->prepare("INSERT INTO compra (piezas,id_stock,id_usuario) VALUES('$pedido','$stock', '$id')");
-            $query2->execute();
-            $query3 =$conn->prepare("INSERT INTO agenda (fecha,servicio,id_usuario) VALUES('$cita','$producto','$id')");
-            $query3->execute();
-           $query4 = "UPDATE stock SET cantidad=$restaStock WHERE id_stock=$stock"; //query para actualizar datos
-            $conn->exec($query4); //ejecuta query
+           if($valor == 0)
+           {
+            echo '<div class="alert alert-success" role="alert">Se realizó la compra correctamente y se agendo la cita en la fecha seleccionada </div>';
+                $query2 =$conn->prepare("INSERT INTO compra (piezas,id_stock,id_usuario) VALUES('$pedido','$stock', '$id')");
+                $query2->execute();
+                $query3 =$conn->prepare("INSERT INTO agenda (fecha,servicio,id_usuario) VALUES('$cita','$producto','$id')");
+                $query3->execute();
+                $query4 = "UPDATE stock SET cantidad=$restaStock WHERE id_stock=$stock"; //query para actualizar datos
+                $conn->exec($query4); //ejecuta query
            }else{
-            $date = $conn->prepare("SELECT fecha FROM agenda");   //lee todos los usuarios de la tabla usuarios
+            $date = $conn->prepare("SELECT *  FROM `agenda` WHERE fecha='$cita' ");   //lee todos los usuarios de la tabla usuarios
             $date->execute();
-           
-            while($resDate = $date->fetch()) {
-                $getDate= $resDate['fecha'];
-             echo $getDate;
+            $resDate = $date->fetch();
+            $getDate= $resDate['fecha'];
+            
              if($cita == $getDate){
-                 echo "fecha ocupada";
+                 echo '<div class="alert alert-danger" role="alert">fecha ocupada, por favor seleccione una fecha diferente</div>';
              }else{
-                 echo "fecha agendada ";
+                echo '<div class="alert alert-success" role="alert">Se realizó la compra correctamente y se agendo la cita en la fecha seleccionada.. </div>';
                  $query2 =$conn->prepare("INSERT INTO compra (piezas,id_stock,id_usuario) VALUES('$pedido','$stock', '$id')");
-                 $query2->execute();
+                $query2->execute();
                  $query3 =$conn->prepare("INSERT INTO agenda (fecha,servicio,id_usuario) VALUES('$cita','$producto','$id')");
                  $query3->execute();
                  $query4 = "UPDATE stock SET cantidad=$restaStock WHERE id_stock=$stock"; //query para actualizar datos
                  $conn->exec($query4); //ejecuta query
                  $query5 =$conn->prepare("INSERT INTO notificacion (asunto,descripcion,id_usuario) VALUES('$cita','$producto','$id')");
-                 $query5->execute();
+                $query5->execute();
 
 
              }
-             }
+             
            }
 
            
            
        }else{
-           echo "tas wey no me alcanza";
-          echo"    <script>";
-          echo"  document.getElementById('res').innerHTML('noooooooooooooooooooooooooo'); ";
-          echo"  </script>";
+           echo  '<div class="alert alert-danger" role="alert">No alcanzan los productos existentes en stock para realizar la compra</div>';
+        
            
        }
    }else{
-       echo "no";
+       echo '<div class="alert alert-danger" role="alert">Lo sentimos... no hay productos en stock</div>';
    }
     
 }else{
